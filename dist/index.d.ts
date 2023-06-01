00823f9f7cd2de99fe212c036069ef3f2db0d88e
abir <izatop@gmail.com>
2023-06-01 17:22:29 +0400

    fix: config

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import { Node, Attribute } from './node';
-import ReadableStream = NodeJS.ReadableStream;
-export declare function convertStream(stream: ReadableStream): Promise<Node>;
-export declare function convertString(XMLString: string): Promise<Node>;
-export { Node, Attribute };
